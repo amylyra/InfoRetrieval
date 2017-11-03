45ab4d7bd8efdf99180e915aba2c529085be81b5
Ouahib El Hanchi <ouahib.el.hanchi@gmail.com>
2017-11-03 00:57:45 +0000

    MakingCosmetics Scrapy project

---
+++ makingcosmetics/makingcosmetics/loaders.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""Item Loaders"""
+
+# Imports =====================================================================
+
+import re
+
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, MapCompose, Join, Identity
+from w3lib.html import remove_tags, replace_entities
+
+# Processors ==================================================================
+
+def clean_text(text):
+    """Clean text from tags, replace entities and normalize whitespaces"""
+    text = remove_tags(unicode(text))
+    text = replace_entities(text)
+    # Normalize whitespace
+    text = re.sub(r'(\s)+', '\\1', text)
+    # Strip whitespace
+    return text.strip()
+
+# Loaders =====================================================================
+
+class ProductItemLoader(ItemLoader):
+    """Product item loader"""
+    default_input_processor = MapCompose(clean_text)
+    default_output_processor = TakeFirst()
+    
+    sizes_out = Identity()
+    category_out = Join(' ')
+    documents_out = Join(', ')
+    more_information_out = Join(', ')
+
+# END =========================================================================
